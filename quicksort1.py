dc0161924367a25089521a7cdc74761234edad17
Gogyoo <Gogyoo@users.noreply.github.com>
2014-11-24 23:09:29 +0100

    Update quicksort1.py
    Doesn't want to accept p +1 or sample - 1 as function arguments, so new variables defined for that.
    
    Also, I now exceed the maximum recursion depth allowed.

--- quicksort1.py
+++ quicksort1.py
@@ -4,6 +4,7 @@ this is an attempt to implement a quicksort algorithm."""
 from random import randint
 
 sample = 100 #replace with a user input if algo works fine
+isample = sample - 1
 
 def jumbled_list (n):
 #creates a list with n RGNs. About 18 s for a million integers.
@@ -13,23 +14,25 @@ def jumbled_list (n):
     print jumbled
     return jumbled
     
-jumbled_list(sample)
+jumbled = jumbled_list(sample)
 
 def partition (array, low, high):
-    pivot = array[sample//2]
-    leftwall = 1
+    pivot = array[-1]
+    lw = 0
     for i in range (0, high-1):#, high) to be tried
         if array[i] < pivot:
-            leftwall += 1
-            array[i], pivot = pivot, array[i]
-        pivot, array[i] = array[i], pivot
-        return pivot
+            lw += 1
+            array[i], array[lw] = array[lw], array[i]
+        pivot, array[lw+1] = array[lw+1], pivot
+        return lw+1
         
 def quicksort (A, low, high):
     if low < high:
         p = partition (A, low, high)
-        quicksort(A, low, p - 1)
-        quicksort(A, p + 1, high)
+        newlow = p + 1
+        newhigh = p - 1
+        quicksort(A, low, newhigh)
+        quicksort(A, newlow, high)
         print A
 
-quicksort (jumbled_list, 0, sample - 1)
+quicksort (jumbled, 0, isample)
